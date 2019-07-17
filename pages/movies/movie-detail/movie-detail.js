b24b2adf68cd0c963e4f6c940dc80f17baabc364
弓舍羊 <785548743@qq.com>
2019-07-17 21:24:00 +0800

    最终版
    部分代码加上ES6

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,6 +1,7 @@
 // pages/movies/movie-detail/movie-detail.js
 var app=getApp();
-var util=require("../../../utils/util.js");
+// var util=require("../../../utils/util.js");
+import {Movie} from 'class/Movie.js';
 Page({
 
 	/**
@@ -17,48 +18,54 @@ Page({
 	onLoad: function (options) {
 		var movieId=options.id;
 		var url=app.globalData.doubanBase+"/v2/movie/subject/"+movieId;
-		util.http(url,this.processDoubanData);
-	},
-	processDoubanData:function(data){
-		if(!data)
-		{
-			return;
-		}
-		var director={
-			avatar:"",
-			name:"",
-			id:""
-		}
-		if(data.directors[0]!=null)
-		{
-			if(data.directors[0].avatars!=null)
-			{
-				director.avatar=data.directors[0].avatars.large;
-			}
-			director.name=data.directors[0].name;
-			director.id=data.directors[0].id;
-		}
-		var movie={
-			movieImg:data.images?data.images.large:"",
-			country:data.countries[0],
-			title:data.title,
-			originalTitle:data.original_title,
-			wishCount: data.wish_count,
-			commentCount:data.comments_count,
-			year:data.year,
-			generes:data.genres.join("、"),
-			stars:util.convertToStarsArray(data.rating.stars),
-			score:data.rating.average,
-			director:director,
-			casts:util.convertToCastString(data.casts),
-			castsInfo:util.convertToCastInfos(data.casts),
-			summary:data.summary
-		}
-		this.setData({
-			movie:movie,
-			title:data.title
+		// util.http(url,this.processDoubanData);
+		var movie = new Movie(url);
+		movie.getMovieData((movie)=>{
+			this.setData({
+				movie:movie
+			});
 		});
 	},
+	// processDoubanData:function(data){
+	// 	if(!data)
+	// 	{
+	// 		return;
+	// 	}
+	// 	var director={
+	// 		avatar:"",
+	// 		name:"",
+	// 		id:""
+	// 	}
+	// 	if(data.directors[0]!=null)
+	// 	{
+	// 		if(data.directors[0].avatars!=null)
+	// 		{
+	// 			director.avatar=data.directors[0].avatars.large;
+	// 		}
+	// 		director.name=data.directors[0].name;
+	// 		director.id=data.directors[0].id;
+	// 	}
+	// 	var movie={
+	// 		movieImg:data.images?data.images.large:"",
+	// 		country:data.countries[0],
+	// 		title:data.title,
+	// 		originalTitle:data.original_title,
+	// 		wishCount: data.wish_count,
+	// 		commentCount:data.comments_count,
+	// 		year:data.year,
+	// 		generes:data.genres.join("、"),
+	// 		stars:util.convertToStarsArray(data.rating.stars),
+	// 		score:data.rating.average,
+	// 		director:director,
+	// 		casts:util.convertToCastString(data.casts),
+	// 		castsInfo:util.convertToCastInfos(data.casts),
+	// 		summary:data.summary
+	// 	}
+	// 	this.setData({
+	// 		movie:movie,
+	// 		title:data.title
+	// 	});
+	// },
 	onReady: function ()
 	{
 		wx.setNavigationBarTitle({
